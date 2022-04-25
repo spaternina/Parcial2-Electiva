8a37671cb99dc8ae0d64456c1e57e25ec2a6b40d
Sebastian Paternina <spaterninachavez@gmail.com>
2022-04-24 20:55:03 -0500

    Agregar archivos de proyecto.

---
+++ Parcial2-Electiva/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Parcial2_Electiva.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
